8a4b3b6655908de11764c61454078c6f96be0706
vntu <vntu98@gmail.com>
2021-08-29 11:51:02 +0700

    handle register new account

---
+++ app/Http/Controllers/Auth/SignUpController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Transformers\Users\UserTransformer;
+use Illuminate\Http\Request;
+
+class SignUpController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|unique:users,email',
+            'username' => 'required|alpha_dash|unique:users,username',
+            'name' => 'required',
+            'password' => 'required|min:6|confirmed',
+            'password_confirmation' => 'required'
+        ]);
+
+        $user = User::create($request->only('email', 'username', 'name', 'password'));
+
+        return fractal()
+            ->item($user)
+            ->transformWith(new UserTransformer())
+            ->toArray();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\SignInController;
 use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\SignOutController;
+use App\Http\Controllers\Auth\SignUpController;
 use App\Http\Controllers\Me\SnippetController as MeSnippetController;
 use App\Http\Controllers\Snippets\SnippetController;
 use App\Http\Controllers\Snippets\StepController;
@@ -11,6 +12,7 @@ use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
+    Route::post('signup', SignUpController::class);
     Route::post('signin', SignInController::class);
     Route::post('signout', SignOutController::class);
     Route::get('me', MeController::class);
